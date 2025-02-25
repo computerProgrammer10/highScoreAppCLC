4026d8eb90a3cd00d01c0f522460c7cddb5442c3
hi <daniel.huseby26@d155.org>
2025-02-25 08:07:32 -0600

    Initial Commit

---
+++ foreverGameCLCAppTests/foreverGameCLCAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  foreverGameCLCAppTests.swift
+//  foreverGameCLCAppTests
+//
+//  Created by DANIEL HUSEBY on 2/25/25.
+//
+
+import Testing
+@testable import foreverGameCLCApp
+
+struct foreverGameCLCAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

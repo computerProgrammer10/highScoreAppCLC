8e303a735d8968a3b17e1a1199d522512ab586d3
Hi <daniel.huseby26@d155.org>
2025-03-05 11:52:45 -0600

    hi

---
+++ foreverGameCLCApp/Obstacle.swift
@@ -0,0 +1,49 @@
+//
+//  Obstacle.swift
+//  foreverGameCLCApp
+//
+//  Created by DANIEL HUSEBY on 3/5/25.
+//
+
+import Foundation
+import SpriteKit
+
+class Obstacle{
+    static var allObstacles = [Obstacle]()
+    
+    // matthew if you're reading this, i am gonna use this to name the obstacles. i was thinking maybe a tutorial obstacle?
+    // i am gonna also insert a function that would return the obstacle by direct name down in the static functions
+    var name: String
+    var node: SKNode
+    var direction: String
+    var difficulty: String
+    
+    init(name: String, node: SKNode, direction: String, difficulty: String) {
+        self.name = name
+        self.node = node
+        self.direction = direction
+        self.difficulty = difficulty
+        Obstacle.allObstacles.append(self)
+    }
+    
+    // All of these random node methods require there to be an obstacle in the first place inside of the allObstacles array
+    // and the one that uses difficulty requires at least 1 obstacle with that difficulty
+    
+    // all of this code is commented out because i am going to wait until later to see how we should implement this
+    
+//    static func getRandomObstacleDifficulty(difficulty: String) -> Obstacle{
+//
+//    }
+//    
+//    static func getRandomObstacle() -> Obstacle{
+//        return allObstacles[Int.random(in: 0..<allObstacles.count)]
+//    }
+    
+//    static func getNodeByName() -> Obstacle{
+//        // write a thing in here that will return an obstacle based on it's name
+//    }
+    
+    
+    
+    
+}
